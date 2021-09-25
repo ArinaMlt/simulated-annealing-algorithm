a87201670c81b6c43010845859494335cc53bed8
ArinaMlt <79594454+ArinaMlt@users.noreply.github.com>
2021-09-25 20:20:22 +0800

    Update BinPack.java

--- Just/BinPack.java
+++ Just/BinPack.java
@@ -10,7 +10,6 @@ public class BinPack {
     public List<Integer> m() {
         List<Integer> wei = new ArrayList<Integer>();
         try {
-//            Scanner s = new Scanner(new FileReader("/Users/arina/Downloads/Алгоритмы/1.txt"));
             Scanner s = new Scanner(new FileReader("/Users/arina/Downloads/Проектирование/gen/src/main/java/Just/Just/in.txt"));
 
             while (s.hasNextInt()) {
